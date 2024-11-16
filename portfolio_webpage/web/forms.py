361aa31b59cefa511ad05b144ce98c0e589f2849
Sheban07 <shebankaingu@gmail.com>
2024-11-16 09:25:49 +0300

    django assessment

--- portfolio_webpage/web/forms.py
+++ portfolio_webpage/web/forms.py
@@ -4,9 +4,6 @@ from  django import forms
 from .models import Contact
 from django import template
 
-def add_class(field, css_class):
-    return field.as_widget(attrs={"class": css_class})
-
 
 
 class ContactForm(forms.ModelForm):
